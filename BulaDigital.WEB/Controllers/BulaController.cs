15528a8b5b44c358ad9674476b6ed5f016fdb042
Vinicius <fnkcontato@gmail.com>
2022-05-12 14:56:15 -0400

    First Commit

---
+++ BulaDigital.WEB/Controllers/BulaController.cs
@@ -0,0 +1,56 @@
+﻿using BulaDigital.WEB.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Web;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BulaDigital.WEB.Controllers
+{
+    public class BulaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string termo)
+        {
+            var EmpInfo = new AnvisaResponse();
+            using (var client = new HttpClient())
+            {
+                //Passing service base url
+                client.BaseAddress = new Uri("https://bula.vercel.app");
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format
+                if (termo.Contains("'") || termo.Contains("--") || termo.Contains(";") || termo.Contains("=") || termo.Contains("<") || termo.Contains("/"))
+                {
+                    TempData["mensagemErro"] = "⚠️ Por favor, não utilize caracteres especiais no campo de busca.";
+                    return RedirectToAction("Index");
+                }
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient
+                HttpResponseMessage Res = await client.GetAsync("/pesquisar?nome="+ termo + "&pagina=1");
+                //Checking the response is successful or not which is sent using HttpClient
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+                    //Deserializing the response recieved from web api and storing into the Employee list
+                    EmpInfo = JsonConvert.DeserializeObject<AnvisaResponse>(EmpResponse);
+                }
+                // if totalElements is 0
+                if (EmpInfo.totalElements == 0)
+                {
+                    TempData["mensagemErro2"] = "⚠️ Ops, parece que nenhum medicamento foi encontrado, digite novamente!";
+                    return RedirectToAction("Index");
+                }
+                //returning the employee list to view
+                return View(EmpInfo);
+
+            }
+        }
+
+    }
+}
---
+++ BulaDigital.WEB/wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+(() => {
+    if (!localStorage.pureJavaScriptCookies) {
+      document.querySelector(".box-cookies").classList.remove('hide');
+    }
+    
+    const acceptCookies = () => {
+      document.querySelector(".box-cookies").classList.add('hide');
+      localStorage.setItem("pureJavaScriptCookies", "accept");
+    };
+    
+    const btnCookies = document.querySelector(".btn-cookies");
+    
+    btnCookies.addEventListener('click', acceptCookies);
+  })();
+
+  (function(d, w, c) {
+    w.ChatraID = 'z26vLtQKgM7wYJxjf';
+    var s = d.createElement('script');
+    w[c] = w[c] || function() {
+        (w[c].q = w[c].q || []).push(arguments);
+    };
+    s.async = true;
+    s.src = 'https://call.chatra.io/chatra.js';
+    if (d.head) d.head.appendChild(s);
+})(document, window, 'Chatra');
